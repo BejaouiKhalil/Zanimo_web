b8757de4dc9a8cfe2972b040c6e3251293ae1988
Unknown <khalil.bejaoui@esprit.tn>
2018-02-02 23:58:03 +0100

    layout admin

--- src/AccessoryBundle/Controller/DefaultController.php
+++ src/AccessoryBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AccessoryBundle:Default:index.html.twig');
+        return $this->render('admin/layout.html.twig');
     }
 }
